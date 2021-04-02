f504e4e301dc876e41d5e11fc26971af2c157455
betcher <betkher.al@mail.ru>
2021-04-02 16:57:41 +0700

    fixes for termhelper.sh

--- profile.d/termhelper.sh
+++ profile.d/termhelper.sh
@@ -3,12 +3,19 @@ command_not_found_handle() {
 	TEXTDOMAINDIR="/usr/share/locale"
 	MSG01="$(gettext -s "command not found")"
     MSG02="$(gettext -s "try to install one of this")"
-    echo "$(basename $0): $MSG01: $1" >&2
+    MSG03="$(gettext -s "Warning:")"
+    MSG04="$(gettext -s "avaliable for root only")"
+    
+    echo "$0: $MSG01: $1" >&2
     if [[ -t 1 ]] ; then
-		# urpmf works like a peice of shit, uncomment this string if you know how to fix it. 
-		#[ -f /usr/bin/urpmf ] && PACKS="$(urpmf --uniq --qf 'urpmi %-30name #found' --files /usr/bin/"$1" /bin/"$1" /sbin/"$1" 2>/dev/null)" 
-		[ -f /usr/bin/dnf ] && 	PACKS=$(dnf repoquery -qC --qf "dnf install %{name}" /usr/bin/$1 /bin/$1 /sbin/$1 2>/dev/null) 
+		if ! echo $PATH |grep -q '/sbin' ; then
+			if find /sbin /usr/sbin -type f -executable -name "$1" |grep -q . ; then
+				echo "${1}: $MSG04" | termhelper - "${MSG03}:"
+				return 127
+			fi
+		fi
+		[ -f /usr/bin/dnf ] && 	PACKS=$(dnf repoquery -qC --qf "dnf install %{name}" /usr/bin/$1 /usr/sbin/$1 /bin/$1 /sbin/$1 2>/dev/null) 
 		[ -n "$PACKS" ] &&   echo "$PACKS" | termhelper - "${MSG02}:"
-    fi
+	fi
     return 127
 } 
