c4f72ad443ceaff1d3c6cbf2ed2f76b2763309c9
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2022-10-31 23:05:30 +0300

    Tune profile script:
    - remove bashisms (any POSIX shell sources /etc/profile.d/*)
    - add shebang to make shellcheck understand that it must check compatibility with POSIX shell
    - respect PATH when checking for dnf
    - avoid exec of grep to save time
    
    Sorry, I made separate commits for each change but accidently lost commits.

--- profile.d/termhelper.sh
+++ profile.d/termhelper.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 command_not_found_handle() {
 	#BINARYSEARCH='no'
 	export TEXTDOMAIN="termhelper"
@@ -9,18 +10,22 @@ command_not_found_handle() {
     MSG05="$(gettext -s "you need root permissions, or try to execute it with full path")"
     
     echo "$0: $MSG01: $1" >&2
-    if [[ -t 1 ]] ; then
+    if [ -t 1 ]; then
 		. /etc/termhelper >/dev/null 2>&1
-		[ "${BINARYSEARCH}_" == 'no_' ] && return 127
-		if ! echo $PATH |grep -q '/sbin' ; then
+		[ "${BINARYSEARCH}_" = 'no_' ] && return 127
+		case "$PATH" in
+		*/sbin* ) : ;;
+		* )
 			found=$(find /sbin /usr/sbin -type f -executable -name "$1")
 			if [ "$found" ] ; then
-				text="$(echo -e "\"$1\" - ${MSG04},\n${MSG05}:\n - ${found}")"
-				echo "$text"  | termhelper - "${MSG03}:"
+				# variable is not add, w/a line breaks
+				text="$(printf "\"%s\" - %s,\n%s:\n - %s" "${1}" "${MSG04}" "${MSG05}" "${found}")"
+				echo "$text" | termhelper - "${MSG03}:"
 				return 127
 			fi
-		fi
-		[ -f /usr/bin/dnf ] && 	PACKS=$(dnf repoquery -qC --qf "sudo dnf install %{name}" /usr/bin/$1 /usr/sbin/$1 /bin/$1 /sbin/$1 2>/dev/null) 
+		;;
+		esac
+		command -v dnf >/dev/null 2>&1 && PACKS="$(dnf repoquery -qC --qf "sudo dnf install %{name}" /usr/bin/$1 /usr/sbin/$1 /bin/$1 /sbin/$1 2>/dev/null)"
 		[ -n "$PACKS" ] &&   echo "$PACKS" | termhelper - "${MSG02}:"
 	fi
     return 127
