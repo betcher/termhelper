d489d64db7f9a5950bf32fca4513b2529f7f4238
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2022-10-31 22:26:46 +0300

    avoid exec of external program id when not needed
    exec syscall takes a lot of time

--- profile.d/termhelper_header.sh
+++ profile.d/termhelper_header.sh
@@ -1,6 +1,6 @@
 [  -f /etc/termhelper ] && . /etc/termhelper >/dev/null 2>&1
 [ -f $HOME/.config/termhelper ] && . $HOME/.config/termhelper >/dev/null  2>&1
-if [ ! "$HELLO" -a "$(id -u)" -ne 0 ] ; then
+if [ ! "$HELLO" -a "$EUID" -ne 0 ] ; then
 	termhelper % header
 fi
 
